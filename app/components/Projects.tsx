813865eea4f90368334432d16b12cc6fc0a9b978
Mariana Caradec <maricaradec@gmail.com>
2025-03-04 10:36:04 -0300

    chore: reorder items for better visual aesthetics

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -8,33 +8,12 @@ const Projects = () => {
         id="projects"
         className="h-screen bg-[#C54402ff] bg-gradient-to-b from-black/70 to-transparent py-20 px-4"
       >
-        <h1 className="text-3xl text-center">My projects</h1>
-        <h2 className="my-10 text-2xl max-w-3xl px-3">
+        <h1 className="text-3xl text-center font-bold">My projects</h1>
+        <h2 className="my-10 text-2xl max-w-3xl px-3 font-bold">
           Here are some of my latest projects:
         </h2>
 
         <div className="flex items-center justify-center gap-3 py-10">
-          <a
-            href="https://marianacaradec.github.io/Pink-Floyd-web/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="relative rounded-lg overflow-hidden">
-              <Image
-                width={300}
-                height={200}
-                src="/pink-floyd-web.png"
-                alt="Pink Floyd web"
-                layout="responsive"
-                className="rounded-lg"
-              />
-
-              <div className="absolute bottom-0 left-0 w-full bg-black/70 font-semi-bold text-lg px-1 rounded-b-lg">
-                Pink Floyd Web
-              </div>
-            </div>
-          </a>
-
           <a
             href="https://meditapp.vercel.app/"
             target="_blank"
@@ -42,7 +21,7 @@ const Projects = () => {
           >
             <div className="relative rounded-lg overflow-hidden">
               <Image
-                width={300}
+                width={250}
                 height={250}
                 src="/meditapp.png"
                 alt="Meditapp"
@@ -57,13 +36,34 @@ const Projects = () => {
           </a>
 
           <a
+            href="https://marianacaradec.github.io/Pink-Floyd-web/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="relative rounded-lg overflow-hidden">
+              <Image
+                width={250}
+                height={200}
+                src="/pink-floyd-web.png"
+                alt="Pink Floyd web"
+                layout="responsive"
+                className="rounded-lg"
+              />
+
+              <div className="absolute bottom-0 left-0 w-full bg-black/70 font-semi-bold text-lg px-1 rounded-b-lg">
+                Pink Floyd Web
+              </div>
+            </div>
+          </a>
+
+          <a
             href="https://zapapps.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <div className="relative rounded-lg overflow-hidden">
               <Image
-                width={300}
+                width={250}
                 height={250}
                 src="/zapapps.png"
                 alt="Zapapps"
